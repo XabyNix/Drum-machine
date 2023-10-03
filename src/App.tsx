878c31c1f483e5063eb71fc7dd7e6236e31144d7
Orazio <xabynix@gmail.com>
2023-10-03 02:18:17 +0000

    added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,92 +12,98 @@ import Pad from "./components/Pad";
 import { useState } from "react";
 
 const pads = [
-	{
-		audioUrl: heater1,
-		id: "heater1",
-		keyButton: "Q",
-	},
-	{
-		audioUrl: heater2,
-		id: "heater2",
-		keyButton: "W",
-	},
-	{
-		audioUrl: heater3,
-		id: "heater3",
-		keyButton: "E",
-	},
-	{
-		audioUrl: heater4,
-		id: "heater4",
-		keyButton: "A",
-	},
-	{
-		audioUrl: heater6,
-		id: "heater6",
-		keyButton: "S",
-	},
-	{
-		audioUrl: kick_n_hat,
-		id: "kick_n_hat",
-		keyButton: "D",
-	},
-	{
-		audioUrl: kick_1,
-		id: "kick_1",
-		keyButton: "Z",
-	},
-	{
-		audioUrl: Cev_H2,
-		id: "Cev_H2",
-		keyButton: "X",
-	},
-	{
-		audioUrl: Disc,
-		id: "Disc",
-		keyButton: "C",
-	},
+  {
+    audioUrl: heater1,
+    id: "heater1",
+    keyButton: "Q",
+  },
+  {
+    audioUrl: heater2,
+    id: "heater2",
+    keyButton: "W",
+  },
+  {
+    audioUrl: heater3,
+    id: "heater3",
+    keyButton: "E",
+  },
+  {
+    audioUrl: heater4,
+    id: "heater4",
+    keyButton: "A",
+  },
+  {
+    audioUrl: heater6,
+    id: "heater6",
+    keyButton: "S",
+  },
+  {
+    audioUrl: kick_n_hat,
+    id: "kick_n_hat",
+    keyButton: "D",
+  },
+  {
+    audioUrl: kick_1,
+    id: "kick_1",
+    keyButton: "Z",
+  },
+  {
+    audioUrl: Cev_H2,
+    id: "Cev_H2",
+    keyButton: "X",
+  },
+  {
+    audioUrl: Disc,
+    id: "Disc",
+    keyButton: "C",
+  },
 ];
 
 function App() {
-	const [display, setDisplay] = useState("");
-	const [isOn, setIsOn] = useState(false);
-	const [volume, setVolume] = useState(0.5);
+  const [display, setDisplay] = useState("");
+  const [isOn, setIsOn] = useState(true);
+  const [volume, setVolume] = useState(0.5);
 
-	function changeDisplay(toDisplay: string) {
-		setDisplay(toDisplay);
-	}
-	return (
-		<div id="container">
-			<div id="drum-machine">
-				<div className="pad-bank">
-					{pads.map((pad) => (
-						<Pad
-							volume={volume}
-							isOn={isOn}
-							changeDisplay={changeDisplay}
-							key={pad.keyButton}
-							{...pad}
-						/>
-					))}
-				</div>
-				<div className="controls-container">
-					<div id="display">{display}</div>
-					<div>
-						<input
-							type="range"
-							min={0}
-							max={100}
-							onChange={(e) => setVolume(parseFloat(e.target.value) / 100)}
-						/>
-					</div>
-					<div className={`control ${isOn ? "on" : "off"}`} onClick={() => setIsOn(!isOn)}>
-						<div className="switch" style={isOn ? { right: 0 } : { right: "70%" }}></div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  function changeDisplay(toDisplay: string) {
+    setDisplay(toDisplay);
+  }
+  return (
+    <div id="container">
+      <div id="drum-machine">
+        <div className="pad-bank">
+          {pads.map((pad) => (
+            <Pad
+              volume={volume}
+              isOn={isOn}
+              changeDisplay={changeDisplay}
+              key={pad.keyButton}
+              {...pad}
+            />
+          ))}
+        </div>
+        <div className="controls-container">
+          <div id="display">{display}</div>
+          <div>
+            <input
+              type="range"
+              min={0}
+              max={100}
+              onChange={(e) => setVolume(parseFloat(e.target.value) / 100)}
+            />
+          </div>
+          <div
+            className={`control ${isOn ? "on" : "off"}`}
+            onClick={() => setIsOn(!isOn)}
+          >
+            <div
+              className="switch"
+              style={isOn ? { right: 0 } : { right: "calc(60px - 20px)" }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -1,47 +1,59 @@
 import { useEffect, useState } from "react";
 
 interface prop {
-	keyButton: string;
-	id: string;
-	audioUrl: string;
-	changeDisplay(audioName: string): void;
-	isOn: boolean;
-	volume: number;
+  keyButton: string;
+  id: string;
+  audioUrl: string;
+  changeDisplay(audioName: string): void;
+  isOn: boolean;
+  volume: number;
 }
 
-export default function Pad({ keyButton, audioUrl, id, changeDisplay, isOn, volume }: prop) {
-	const [isPlaying, setIsPlaying] = useState(false);
-	function playAudio() {
-		if (isOn) {
-			const audioTag = document.getElementById(keyButton) as HTMLAudioElement;
-			changeDisplay(id);
-			audioTag.currentTime = 0;
-			audioTag.volume = volume;
-			audioTag.play();
-			setIsPlaying(true);
+export default function Pad({
+  keyButton,
+  audioUrl,
+  id,
+  changeDisplay,
+  isOn,
+  volume,
+}: prop) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  function playAudio() {
+    if (isOn) {
+      const audioTag = document.getElementById(keyButton) as HTMLAudioElement;
+      changeDisplay(id);
+      audioTag.currentTime = 0;
+      audioTag.volume = volume;
+      audioTag.play();
+      setIsPlaying(true);
 
-			setTimeout(() => {
-				setIsPlaying(false);
-			}, 150);
-		}
-	}
-	function handleKeydown(event: globalThis.KeyboardEvent) {
-		event.key.toLowerCase() === keyButton.toLowerCase() && playAudio();
-	}
+      setTimeout(() => {
+        setIsPlaying(false);
+      }, 150);
+    }
+  }
+  function handleKeydown(event: globalThis.KeyboardEvent) {
+    event.key.toLowerCase() === keyButton.toLowerCase() && playAudio();
+  }
 
-	useEffect(() => {
-		document.addEventListener("keypress", handleKeydown);
-		return () => document.removeEventListener("keypress", handleKeydown);
-	});
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeydown);
+    return () => document.removeEventListener("keypress", handleKeydown);
+  });
 
-	return (
-		<button
-			className={`drum-pad ${isPlaying ? "playing" : ""}`}
-			id={id}
-			onClick={() => playAudio()}
-		>
-			{keyButton}
-			<audio preload="none" className="clip" id={keyButton} src={audioUrl}></audio>
-		</button>
-	);
+  return (
+    <button
+      className={`drum-pad ${isPlaying ? "playing" : ""}`}
+      id={id}
+      onMouseDown={() => playAudio()}
+    >
+      {keyButton}
+      <audio
+        preload="none"
+        className="clip"
+        id={keyButton}
+        src={audioUrl}
+      ></audio>
+    </button>
+  );
 }
